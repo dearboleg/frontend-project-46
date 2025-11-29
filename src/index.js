56b31f62e45d66f41ff0dd5c6c47d9a0bd5b0aec
Oleg Bogachev <bolegcarrick@gmail.com>
2025-11-29 22:38:35 +0300

    new comparison logic added to src/index.js and asciinema gendiff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import parse from "./parsers.js"
+import parse from "./parsers.js";
+import _ from "lodash";
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = parse(filepath1);
   const data2 = parse(filepath2);
-  
-  return `Difference between ${data1} and ${data2}`;
+
+  const dataKeys1 = Object.keys(data1);
+  const dataKeys2 = Object.keys(data2);
+  const allDataKeys = _.sortBy(_.union(dataKeys1, dataKeys2));
+
+  const difference = allDataKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
+    if (!_.has(data2, key)) {
+      return ` - ${key}: ${value1}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${value2}`;
+    }
+    if (!_.isEqual(value1, value2)) {
+      return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
+    }
+    return `    ${key}: ${value1}`;
+  });
+
+  return `{\n${difference.join("\n")}\n}`;
 };
 
 export default genDiff;
